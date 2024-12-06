ae1d80f2f5743c2522019449ff7d94694f03dfe5
Kevin Schmiederer <kevin.schmiederer@dungeon-tinker.com>
2024-12-06 13:25:35 +0100

    optimized

--- code/day6/partOne.py
+++ code/day6/partOne.py
@@ -1,25 +1,18 @@
 matrix = [list(line) for line in open("input.txt", "r").read().splitlines()]
 
-direction_map = {'^': '>', '>': 'v', 'v': '<', '<': '^'}
-
 def move(direction, pos):
     matrix[pos[0]][pos[1]] = 'X'
-    
     offsets = {'^': (-1, 0), '<': (0, -1), '>': (0, 1), 'v': (1, 0)}
     new_pos = [pos[0] + offsets[direction][0], pos[1] + offsets[direction][1]]
-    
-    # Check if the new position is within bounds
+
     if not (0 <= new_pos[0] < len(matrix)) or not (0 <= new_pos[1] < len(matrix[0])):
         return "NONE", new_pos
 
     if matrix[new_pos[0]][new_pos[1]] != '#':
         matrix[new_pos[0]][new_pos[1]] = direction
         return direction, new_pos
-    else: #only turn, no movement
-        direction = direction_map.get(direction)
-        return direction, pos  
+    else: return {'^': '>', '>': 'v', 'v': '<', '<': '^'}.get(direction), pos  #only turn, no movement 
 
-direction = "^"
-pos = next((r, row.index(direction)) for r, row in enumerate(matrix) if direction in row)
+pos = next((r, row.index(direction)) for r, row in enumerate(matrix) if (direction := "^") in row)
 while direction != "NONE": direction, pos = move(direction, pos)
 print(sum(row.count('X') for row in matrix))
